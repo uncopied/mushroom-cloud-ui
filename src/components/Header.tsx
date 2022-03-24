5991528cc382f8cd515a0999f778a6f110df3925
fei <f3i.g4o@gmail.com>
2022-03-24 15:50:03 -0400

    first commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import logo from "../../src/logo.svg";
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Mushroom Cloud NFT</h1>
+      </header>
+      //   <header className="App-header">
+      //     <img src={logo} className="App-logo" alt="logo" />
+      //     <p>
+      //       Edit <code>src/App.js</code> and save to reload.
+      //     </p>
+      //     <a
+      //       className="App-link"
+      //       href="https://reactjs.org"
+      //       target="_blank"
+      //       rel="noopener noreferrer"
+      //     >
+      //       Learn React
+      //     </a>
+      //   </header>
+    );
+  }
+}
+
+export default Header;
