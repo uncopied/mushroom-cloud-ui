733575b7fcfe06f799b920c6bdf5f75e806ed5b9
fei <f3i.g4o@gmail.com>
2022-03-25 15:35:01 -0400

    add algorand wallet

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import React from "react";
-import "./App.css";
-import Header from "./components/Header";
+import React from 'react';
+import './App.css';
+import Header from './components/Header';
+import WalletConnectButton from './components/WalletConnectButton';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Header></Header>
+      <WalletConnectButton></WalletConnectButton>
     </div>
   );
 }
